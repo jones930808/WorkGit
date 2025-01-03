8ad00dddfb922d5e84562fc2afa20422573f1bfd
jones930808 <15862406960@163.com>
2025-01-03 09:49:57 +0800

    Initial commit

---
+++ IoTDataAuto/config/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author  : WuJiLiu
+# @Pseudonym : ToyRocket 
+# @FileName: __init__.py.py
+# @Software: PyCharm
+# @Time    : 2025/1/2 18:08
+# @Email   : 1151905906@qq.com
